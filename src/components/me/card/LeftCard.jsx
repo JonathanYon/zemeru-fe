9cc9f9280652c68fe5a745be2c954ca64ea2c80d
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-18 23:54:10 +0200

    simple card

---
+++ src/components/me/card/LeftCard.jsx
@@ -0,0 +1,11 @@
+import { Card } from "react-bootstrap";
+import "./left-card.css";
+
+const LeftCard = () => {
+  return (
+    <Card className="left-card">
+      <Card.Body>hi</Card.Body>
+    </Card>
+  );
+};
+export default LeftCard;
