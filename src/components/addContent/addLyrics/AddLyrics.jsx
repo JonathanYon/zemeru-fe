68a5b1089832a52f467aeeee6101d51082a1f6db
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-19 20:56:50 +0200

    lyrics form

--- src/components/addContent/addLyrics/AddLyrics.jsx
+++ src/components/addContent/addLyrics/AddLyrics.jsx
@@ -1,4 +1,5 @@
 import { Col, Container, Form, Row } from "react-bootstrap";
+import TextareaAutosize from "react-textarea-autosize";
 
 const AddLyrics = () => {
   return (
@@ -13,6 +14,20 @@ const AddLyrics = () => {
             <Form.Group controlId="formGroupTitle">
               <Form.Label>Title</Form.Label>
               <Form.Control type="text" placeholder="Title" />
+            </Form.Group>
+            <Form.Group
+              controlId="exampleForm.ControlTextarea1"
+              className="d-flex flex-column"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <TextareaAutosize minRows={10} />
+            </Form.Group>
+            <Form.Group controlId="formGroupTitle">
+              <Form.Label>YouTube Link</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="www.youtube.com/3223bjik"
+              />
             </Form.Group>
           </Form>
         </Col>
