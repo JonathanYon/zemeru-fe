caa0f63dde00b419e4da6cd703a2b0f81851e48f
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-25 16:51:33 +0200

    sign up form

---
+++ src/components/signUp/SignUp.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+const SignUp = () => {
+  return (
+    <Form>
+      <Form.Group controlId="formBasicUserName">
+        <Form.Label>User name</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="UserName"
+          id="userName"
+          value={info.username}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          id="email"
+          value={info.email}
+          onChange={handleChange}
+        />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          id="password"
+          value={info.password}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+export default SignUp;
