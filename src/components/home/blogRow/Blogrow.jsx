0aadd569ab7f10b02f8aed53a64d29328e14f9df
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-26 11:01:40 +0200

    fetching blogs

--- src/components/home/blogRow/Blogrow.jsx
+++ src/components/home/blogRow/Blogrow.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import SingleBlog from "../single blog/SingleBlog";
 
 const BlogRow = () => {
+  const [blog, setBlog] = useState([]);
+  useEffect(() => {
+    const blogPosts = async () => {
+      const response = await fetch(`${process.env.REACT_APP_URL}/blogs`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${window.localStorage.getItem("Token")}`,
+        },
+      });
+      if (response.ok) {
+        const res = await response.json();
+        console.log(res);
+        setBlog(res);
+      }
+    };
+    blogPosts();
+  }, []);
+
   return (
     <Container className="mt-5">
       <Row xs={4}>
         <Col>
-          <SingleBlog />
+          {blog.map((blogData) => (
+            <SingleBlog blog={blogData} key={blogData._id} />
+          ))}
         </Col>
       </Row>
     </Container>
