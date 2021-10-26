9c77a3fbce3d38d8c6a42651f421f98051d18c95
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-26 22:22:50 +0200

    store+provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import configStore from "./Redux/store";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={configStore}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
