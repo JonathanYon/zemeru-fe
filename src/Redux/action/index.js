3393ddc0eee201718938b7c71588faa9b148b9ab
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-27 02:20:59 +0200

    blogAction

--- src/Redux/action/index.js
+++ src/Redux/action/index.js
@@ -1,6 +1,9 @@
 export const LOG_ME = "LOG_ME";
 export const ERROR_ME = "ERROR_ME";
 export const LOADING_ME = "LOADING_ME";
+export const LOADING_BLOG = "LOADING_BLOG";
+export const ERROR_BLOG = "ERROR_BLOG";
+export const BLOGS = "BLOGS";
 
 export const myLogin = () => {
   return async (dispatch) => {
@@ -31,6 +34,41 @@ export const myLogin = () => {
       console.log(error);
       dispatch({
         type: ERROR_ME,
+        payload: true,
+      });
+    }
+  };
+};
+//blogs
+export const myBlogs = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_URL}/blogs`, {
+        headers: {
+          Authorization: `Bearer ${window.localStorage.getItem("Token")}`,
+        },
+      });
+      if (response.ok) {
+        const res = await response.json();
+        console.log(res);
+        dispatch({
+          type: LOADING_BLOG,
+          payload: false,
+        });
+        dispatch({
+          type: BLOGS,
+          payload: res,
+        });
+      } else {
+        dispatch({
+          type: ERROR_BLOG,
+          payload: true,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      dispatch({
+        type: ERROR_BLOG,
         payload: true,
       });
     }
