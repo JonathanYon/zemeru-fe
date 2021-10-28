4ace46fd8a30ffb0d2a0b87ff7b788d421d1c933
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-28 17:44:16 +0200

    get Single Lyrics

--- src/components/addContent/lyrics/Lyrics.jsx
+++ src/components/addContent/lyrics/Lyrics.jsx
@@ -1,8 +1,34 @@
-const Lyrics = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+import { useEffect, useState } from "react";
+import { Container, Row, Col, Jumbotron } from "react-bootstrap";
+import { withRouter } from "react-router";
+import { FaThumbsUp } from "react-icons/fa";
+import "react-quill/dist/quill.snow.css";
+import ReactQuill from "react-quill";
+
+const Lyrics = (props) => {
+  const [lyric, setLyric] = useState(null);
+
+  const { id } = props.match.params;
+
+  useEffect(() => {
+    const getLyric = async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_URL}/lyrics/${id}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${window.localStorage.getItem("Token")}`,
+          },
+        }
+      );
+      if (response.ok) {
+        const res = await response.json();
+        console.log("Lyric,jsx", res);
+        setLyric(res);
+      }
+    };
+    getLyric();
+  }, [id]);
+  return <>hello</>;
 };
-export default Lyrics;
+export default withRouter(Lyrics);
