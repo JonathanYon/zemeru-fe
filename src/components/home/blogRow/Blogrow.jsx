f2af7742e6693392e99908f1ec84628ea124f213
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-28 15:56:33 +0200

    blog big page

--- src/components/home/blogRow/Blogrow.jsx
+++ src/components/home/blogRow/Blogrow.jsx
@@ -3,11 +3,13 @@ import { Col, Container, Row } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { myBlogs } from "../../../Redux/action";
-import SingleBlog from "../single blog/SingleBlog";
-
+import LyricsChart from "../charts/lyrics chart/LyricsChart";
+import UserChart from "../charts/user-chart/UserChart";
+import SingleBlog from "./single-blog/SingleBlog";
 const BlogRow = () => {
   const blogs = useSelector((state) => state.blogs.articles);
-  console.log(blogs);
+  const me = useSelector((state) => state.user.me);
+  // console.log(blogs);
   const dispatch = useDispatch();
   // const [blog, setBlog] = useState([]);
   useEffect(() => {
@@ -30,13 +32,15 @@ const BlogRow = () => {
 
   return (
     <Container className="mt-5">
-      <Row xs={5} className="d-md-flex">
+      <Row md={5} sm={4} className="d-md-flex">
         {blogs.map((blogData) => (
           // <Col className="d-md-flex" key={blogData._id}>
           <SingleBlog blog={blogData} />
           /* </Col> */
         ))}
       </Row>
+      <LyricsChart />
+      <UserChart />
     </Container>
   );
 };
