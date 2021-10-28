990952bab1c60c3acce000f532c9d75ac811b654
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-28 15:58:22 +0200

    me page

--- src/components/me/Me.jsx
+++ src/components/me/Me.jsx
@@ -1,8 +1,10 @@
 import { Jumbotron, Container, Row, Col, Button } from "react-bootstrap";
+import { useSelector } from "react-redux";
 import LeftCard from "./card/LeftCard";
 import "./me.css";
 
 const Me = () => {
+  const me = useSelector((state) => state.user.me);
   return (
     <>
       <Jumbotron
@@ -14,7 +16,7 @@ const Me = () => {
           backgroundSize: "100% 100%",
         }}
       >
-        <Container className="text-white position-relative">
+        <Container className="text-black position-relative">
           <h1>Fluid jumbotron</h1>
           <p>
             This is a modified jumbotron that occupies the entire horizontal
@@ -22,13 +24,14 @@ const Me = () => {
           </p>
           <div className="my-photo ">
             <img
-              src="https://www.gannett-cdn.com/-mm-/fd5c5b5393c72a785789f0cd5bd20acedd2d2804/c=0-350-2659-1850/local/-/media/Phoenix/BillGoodykoontz/2014/04/24//1398388295000-Homer-Simpson.jpg"
+              // src="https://www.gannett-cdn.com/-mm-/fd5c5b5393c72a785789f0cd5bd20acedd2d2804/c=0-350-2659-1850/local/-/media/Phoenix/BillGoodykoontz/2014/04/24//1398388295000-Homer-Simpson.jpg"
+              src={me.cover}
               alt=""
-              className="photo "
+              className="photo"
             />
           </div>
         </Container>
-        <div className="d-flex follow text-white">
+        <div className="d-flex follow text-black">
           <small className="pr-4">Featured</small>
           <small className="pr-4">Followers</small>
           <small className="pr-4">Following</small>
@@ -38,7 +41,7 @@ const Me = () => {
       <Container>
         <Row>
           <Col xs={4}>
-            <h5>@yoni</h5>
+            <h5>@{me.username}</h5>
             <Button className="bg-light text-black-50 mb-2">Edit</Button>
             <LeftCard />
             <LeftCard />
