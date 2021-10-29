bca6c7d01357fc1855b8d8c12806239e2f4e0471
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-30 01:44:34 +0200

    oneComment modified

--- src/components/home/comments/OneComment.jsx
+++ src/components/home/comments/OneComment.jsx
@@ -1,25 +1,27 @@
+import { useState } from "react";
 import { FaThumbsUp } from "react-icons/fa";
 import { FiEdit2 } from "react-icons/fi";
 import { MdDeleteForever } from "react-icons/md";
+import { withRouter } from "react-router";
 
-const OneComment = () => {
+const OneComment = ({ comment }) => {
   return (
     <div className="card mb-4">
       <div className="card-body">
-        <p>Type your note, and hit enter to add it</p>
+        <p>{comment.comment}</p>
 
         <div className="d-flex justify-content-between">
           <div className="d-flex flex-row align-items-center">
             <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(4).jpg"
+              // src="https://mdbootstrap.com/img/Photos/Avatars/img%20(4).jpg"
+              src={comment.userId.avatar}
               alt="avatar"
               className="rounded-circle mr-1"
-              style={{ width: "25px", height: "25px" }}
+              style={{ width: "25px", height: "25px", objectFit: "cover" }}
             />
             <p className="small mb-0 ms-2">Martha</p>
           </div>
           <div className="d-flex flex-row align-items-center">
-            <p className="small text-muted mb-0">Upvote?</p>
             <FaThumbsUp className="mx-2" style={{ marginTop: "-0.16rem" }} />
             <p className="small text-muted mb-0">3</p>
             <FiEdit2 className="mx-1" />
@@ -30,4 +32,4 @@ const OneComment = () => {
     </div>
   );
 };
-export default OneComment;
+export default withRouter(OneComment);
