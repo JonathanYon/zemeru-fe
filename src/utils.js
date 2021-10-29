a154780bd98217d70ca200b887f4d1e499b5b3e3
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-29 23:15:59 +0200

    current user

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+export const jwtId = (token) => {
+  if (!token) {
+    return;
+  }
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace("-", "+").replace("_", "/");
+  return JSON.parse(window.atob(base64));
+};
