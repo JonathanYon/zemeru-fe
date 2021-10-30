4cc5cdfa3108422929ab5c17da89e136d05ba90e
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-31 00:55:01 +0200

    me

--- src/components/me/Me.jsx
+++ src/components/me/Me.jsx
@@ -8,22 +8,21 @@ const Me = ({ me }) => {
       <Jumbotron
         fluid
         style={{
-          backgroundImage:
-            "url('https://lh3.googleusercontent.com/proxy/7jaQrdQKDIa71rC-jnVLbw8o8BqHf1YJwoWvB96fbB91Z2aMDn6bQOADuZeJut2zZbBqolkTR_BmlawOIsx7AWh1wpApPP3QJikUtObsz2Ro_DHgGTRpj1zoNe030bjY8CUty32ey2NaRKTMNYJnl9RmC93s0Y5rh3c6')",
+          backgroundImage: `url(${me.avatar})`,
           backgroundRepeat: "repeat-x",
           backgroundSize: "100% 100%",
         }}
       >
         <Container className="text-black position-relative">
           <h1>Fluid jumbotron</h1>
-          <p>
+          {/* <p>
             This is a modified jumbotron that occupies the entire horizontal
             space of its parent.
-          </p>
+          </p> */}
           <div className="my-photo ">
             <img
               // src="https://www.gannett-cdn.com/-mm-/fd5c5b5393c72a785789f0cd5bd20acedd2d2804/c=0-350-2659-1850/local/-/media/Phoenix/BillGoodykoontz/2014/04/24//1398388295000-Homer-Simpson.jpg"
-              src={me?.cover}
+              src={me?.avatar}
               alt=""
               className="photo"
             />
@@ -36,7 +35,7 @@ const Me = ({ me }) => {
         </div>
       </Jumbotron>
 
-      <Container>
+      <Container className="mt-5">
         <Row>
           <Col xs={4}>
             <h5>@{me?.username}</h5>
