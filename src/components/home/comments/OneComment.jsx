4015fbe5b20c6071e616ab8ff071d928fcc0b7f9
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-31 00:54:40 +0200

    apdate comment work from FE but a bug in the BE needs to be fixed

--- src/components/home/comments/OneComment.jsx
+++ src/components/home/comments/OneComment.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from "react-bootstrap";
 import { FaThumbsUp } from "react-icons/fa";
 import { FiEdit2 } from "react-icons/fi";
 import { MdDeleteForever } from "react-icons/md";
@@ -6,11 +7,13 @@ import { useSelector } from "react-redux";
 import { withRouter } from "react-router";
 
 const OneComment = ({ match, comment }) => {
+  const [update, setUpdate] = useState(false);
+  const [editComment, setEditComment] = useState(comment.comment);
   const me = useSelector((state) => state.user.me._id);
-  const deleteComment = async (commentID) => {
-    const { id } = match.params;
 
+  const deleteComment = async (commentID) => {
     try {
+      const { id } = match.params;
       const response = await fetch(
         `${process.env.REACT_APP_URL}/lyrics/post/${id}/comments/${commentID}`,
         {
@@ -31,34 +34,103 @@ const OneComment = ({ match, comment }) => {
       console.log(error);
     }
   };
+
+  //---------update
+
+  const updateComment = async (commentID) => {
+    // e.preventDefault();
+    try {
+      const payload = {
+        comment: editComment,
+      };
+      const { id } = match.params;
+      const response = await fetch(
+        `${process.env.REACT_APP_URL}/lyrics/post/${id}/comments/${commentID}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${window.localStorage.getItem("Token")}`,
+          },
+          method: "PUT",
+          body: JSON.stringify(payload),
+        }
+      );
+      if (response.ok) {
+        alert("Update");
+        // const resp = await response.json()
+      } else {
+        alert("somethin wrong in update");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   console.log(comment);
   return (
-    <div className="card mb-4">
-      <div className="card-body">
-        <p>{comment.comment}</p>
+    <>
+      {update ? (
+        <>
+          {/* <form
+        onSubmit={() => updateComment(comment._id)}
+        > */}
+          <input
+            type="text"
+            id="editComment"
+            className="form-control"
+            placeholder="Type comment..."
+            value={editComment}
+            onChange={(e) => setEditComment(e.target.value)}
+          />
+          <Button
+            className="form-label mt-2"
+            for="addANote"
+            onClick={() => updateComment(comment._id)}
+          >
+            Edit
+          </Button>
+          <Button
+            className="form-label mt-2 ml-1"
+            onClick={() => setUpdate(false)}
+          >
+            cancel
+          </Button>
+          {/* </form> */}
+        </>
+      ) : (
+        <div className="card mb-4">
+          <div className="card-body">
+            <p>{comment.comment}</p>
 
-        <div className="d-flex justify-content-between">
-          <div className="d-flex flex-row align-items-center">
-            <img
-              // src="https://mdbootstrap.com/img/Photos/Avatars/img%20(4).jpg"
-              src={comment.userId.avatar}
-              alt="avatar"
-              className="rounded-circle mr-1"
-              style={{ width: "25px", height: "25px", objectFit: "cover" }}
-            />
-            <p className="small mb-0 ms-2">{comment.userId.username}</p>
-          </div>
-          <div className="d-flex flex-row align-items-center">
-            <FaThumbsUp className="mx-2" style={{ marginTop: "-0.16rem" }} />
-            <p className="small text-muted mb-0">3</p>
-            {me === comment.userId._id && <FiEdit2 className="mx-1" />}
-            {me === comment.userId._id && (
-              <MdDeleteForever onClick={() => deleteComment(comment._id)} />
-            )}
+            <div className="d-flex justify-content-between">
+              <div className="d-flex flex-row align-items-center">
+                <img
+                  // src="https://mdbootstrap.com/img/Photos/Avatars/img%20(4).jpg"
+                  src={comment.userId.avatar}
+                  alt="avatar"
+                  className="rounded-circle mr-1"
+                  style={{ width: "25px", height: "25px", objectFit: "cover" }}
+                />
+                <p className="small mb-0 ms-2">{comment.userId.username}</p>
+              </div>
+              <div className="d-flex flex-row align-items-center">
+                <FaThumbsUp
+                  className="mx-2"
+                  style={{ marginTop: "-0.16rem" }}
+                />
+                <p className="small text-muted mb-0">3</p>
+                {me === comment.userId._id && (
+                  <FiEdit2 className="mx-1" onClick={() => setUpdate(true)} />
+                )}
+                {me === comment.userId._id && (
+                  <MdDeleteForever onClick={() => deleteComment(comment._id)} />
+                )}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 export default withRouter(OneComment);
