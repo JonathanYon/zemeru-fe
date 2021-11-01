c69e268d96028a64add2192c4479b7c360197b10
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-01 19:08:17 +0100

    Youtube video added to lyrics

---
+++ src/components/addContent/lyrics/YoutubeVideo.jsx
@@ -0,0 +1,22 @@
+import { Col } from "react-bootstrap";
+
+const YoutubeVideo = ({ ytVideo }) => {
+  //   console.log(ytVideo);
+  const YTcode = ytVideo.split("=")[1];
+
+  console.log(YTcode);
+  return (
+    <Col className="mt-5 ml-n3">
+      <iframe
+        width="540"
+        height="315"
+        src={`https://www.youtube.com/embed/${YTcode}?start=1`}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </Col>
+  );
+};
+export default YoutubeVideo;
