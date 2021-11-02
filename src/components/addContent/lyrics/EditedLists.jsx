c814d98ca11a5ceb12de49a5ba28266a53e90ee2
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-02 03:06:45 +0100

    Update and edit working

---
+++ src/components/addContent/lyrics/EditedLists.jsx
@@ -0,0 +1,70 @@
+import { Button } from "react-bootstrap";
+import { withRouter } from "react-router";
+
+const EditedLists = ({ match }) => {
+  const deleteUserEdit = async () => {
+    //edited/:lId/lyrics/:eId
+    const { lId, eId } = match.params;
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_URL}/lyrics/approve/${lId}/admin/${eId}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${window.localStorage.getItem("Token")}`,
+          },
+        }
+      );
+      if (response.ok) {
+        alert("Deleted");
+      } else {
+        alert("something wrong");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //-------------------------
+  const updateUserEdit = async () => {
+    //edited/:lId/lyrics/:eId
+    const { lId, eId } = match.params;
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_URL}/lyrics/approve/${lId}/admin/${eId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${window.localStorage.getItem("Token")}`,
+          },
+        }
+      );
+      if (response.ok) {
+        alert("Updated");
+      } else {
+        alert("something wrong");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <h3>click blow to Approve and modify the Lyrics</h3>
+        <Button className="bg-success" onClick={updateUserEdit}>
+          Approve
+        </Button>
+      </div>
+      <div>
+        <h3>click blow to Reject and Delete the proposed Edit by the user.</h3>
+        <Button className="bg-danger" onClick={deleteUserEdit}>
+          Reject
+        </Button>
+      </div>
+    </div>
+  );
+};
+export default withRouter(EditedLists);
