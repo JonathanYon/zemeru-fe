ea570835612774a98abe1ecb4dd251853c755a92
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-04 00:31:02 +0100

    singleBlog coming together now

--- src/components/home/blogRow/single-blog/SingleBlog.jsx
+++ src/components/home/blogRow/single-blog/SingleBlog.jsx
@@ -1,5 +1,5 @@
 import "./single-blog.css";
-import { Card, Col, Row } from "react-bootstrap";
+import { Badge, Card, Col, Row } from "react-bootstrap";
 import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
 import { format } from "date-fns";
@@ -12,18 +12,27 @@ const SingleBlog = ({ blog }) => {
       // style={{ width: "18rem" }}
       className="ml-5 mb-4 card-container bg-transparent"
     >
-      <Card.Img
-        variant="top"
-        src={blog.cover}
-        className="front"
-        style={{ objectFit: "cover" }}
-      />
-
+      <div className="position-relative holder">
+        <Card.Img
+          variant="top"
+          src={blog.cover}
+          className="front"
+          style={{ objectFit: "cover" }}
+        />
+        <h6 className="blog-title">{blog.title}</h6>
+        <Badge variant="warning" className="blog-category">
+          {blog.category}
+        </Badge>
+      </div>
       <Link to={`/blogs/${blog._id}`}>
         <Card.Body className="back">
           {/* <Card.Title className="title">{blog.title}</Card.Title> */}
           <Card.Title>{blog.title}</Card.Title>
-          <small>
+          <div
+            className="blog-words"
+            dangerouslySetInnerHTML={{ __html: blog.content }}
+          ></div>
+          <small className="blog-author">
             by <strong>{blog.authors[0].username} </strong>
             <br />
             <small>{format(new Date(blog.createdAt), "PP")}</small>
