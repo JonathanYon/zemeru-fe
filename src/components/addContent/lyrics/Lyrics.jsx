2c4173407073aadd90166a2c032acc1cb9ade370
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-03 02:09:23 +0100

    single Lyrics page with style

--- src/components/addContent/lyrics/Lyrics.jsx
+++ src/components/addContent/lyrics/Lyrics.jsx
@@ -5,6 +5,7 @@ import ContentEditable from "react-contenteditable";
 import { jwtId } from "../../../utils";
 import Comments from "../../home/comments/Comments";
 import YoutubeVideo from "./YoutubeVideo";
+import "./add-lyrics.css";
 
 const Lyrics = (props) => {
   const [lyric, setLyric] = useState(null);
@@ -65,53 +66,125 @@ const Lyrics = (props) => {
   // console.log("YT", yt);
   return (
     <>
-      <Jumbotron
-        fluid
-        style={{
+      {/* style={{
           backgroundImage: `url(${process.env.PUBLIC_URL}/logo.svg)`,
+          backgroundRepeat: "repeat-x",
+          backgroundSize: "100% 100%",
+          objectFit: "cover",
+        }} */}
+
+      {/* <Jumbotron fluid>
+          <Container className="text-black position-relative">
+            <ul>
+              <li>መዝሙር: {lyric?.title}</li>
+              <li>ዘማሪ/ት: {lyric?.artist}</li>
+              <li>ኣስፈርቲ ግጥሚ: {lyric?.userId.username}</li>
+            </ul>
+            <div className="my-photo ">
+              <img
+                src={lyric?.coverImage}
+                alt=""
+                className="photo"
+                style={{
+                  objectFit: "cover",
+                }}
+              />
+            </div>
+          </Container>
+          <div className="d-flex follow text-black">
+            <small className="pr-4">Featured</small>
+            <small className="pr-4">Followers</small>
+            <small className="pr-4">Following</small>
+          </div>
+        </Jumbotron> */}
+
+      <Container
+        fluid
+        className=" big-info-container"
+        style={{
+          backgroundImage: `url(${lyric?.coverImage})`,
           backgroundRepeat: "repeat-x",
           backgroundSize: "100% 100%",
           objectFit: "cover",
         }}
       >
-        <Container className="text-black position-relative">
-          <ul>
-            <li>መዝሙር: {lyric?.title}</li>
-            <li>ዘማሪ/ት: {lyric?.artist}</li>
-            <li>ኣስፈርቲ ግጥሚ: {lyric?.userId.username}</li>
-          </ul>
-          <div className="my-photo ">
-            <img
-              //   src="https://www.gannett-cdn.com/-mm-/fd5c5b5393c72a785789f0cd5bd20acedd2d2804/c=0-350-2659-1850/local/-/media/Phoenix/BillGoodykoontz/2014/04/24//1398388295000-Homer-Simpson.jpg"
-              src={lyric?.coverImage}
-              alt=""
-              className="photo"
-              style={{
-                objectFit: "cover",
-              }}
-            />
-          </div>
-        </Container>
-        <div className="d-flex follow text-black">
-          <small className="pr-4">Featured</small>
-          <small className="pr-4">Followers</small>
-          <small className="pr-4">Following</small>
-        </div>
-      </Jumbotron>
+        <Container className="info-container">
+          <Row style={{ height: "40vh" }}>
+            <Col>
+              <Col className="position-relative d-flex">
+                {/* <div>
+                  <img
+                    src={lyric?.coverImage}
+                    alt=""
+                    className="mezmur-cover mt-5"
+                    style={{ height: "14vw", width: "14vw" }}
+                  />
+                </div> */}
+                <div className="text-white mt-4 d-flex">
+                  <img
+                    src={lyric?.coverImage}
+                    alt=""
+                    // className="mezmur-cover mt-5"
+                    style={{ height: "14vw", width: "14vw" }}
+                  />
+                  <div className="text-left ml-3">
+                    <h2 className="font-weight-bold">{lyric?.title}</h2>
+                    <h3>{lyric?.artist}</h3>
 
-      <Container className="mb-5">
-        <Row>
+                    <img
+                      src={lyric?.userId.avatar}
+                      className="rounded-circle mr-1"
+                      width="25"
+                      height="25"
+                      alt=""
+                    />
+                    <strong>
+                      <small className="text-white-50 font-weight-bold">
+                        Transcribed by:{" "}
+                      </small>
+                      {lyric?.userId.username}
+                    </strong>
+                    <br />
+                    <strong>
+                      <small className="text-white-50">
+                        Number of proposed correction on this Lyrics:{" "}
+                      </small>
+                      {lyric?.editedLyrics.length}
+                    </strong>
+                  </div>
+                </div>
+              </Col>
+            </Col>
+            {/* <Col>yes</Col> */}
+          </Row>
+        </Container>
+      </Container>
+
+      <Container className="my-5">
+        <Row className="d-sm-flex">
           <Col xs={6}>
             {edit ? (
-              <Button variant="secondary" onClick={() => setEdit(!edit)}>
+              <Button
+                variant="light"
+                className="border-dark font-weight-bold"
+                onClick={() => setEdit(!edit)}
+              >
                 Edit Lyrics
               </Button>
             ) : (
               <div>
-                <Button variant="success" onClick={editLyrics}>
+                <Button
+                  variant="light"
+                  className="mr-2 text-success border-success font-weight-bold"
+                  onClick={editLyrics}
+                >
                   Propose correction
                 </Button>
-                <Button variant="dark" onClick={() => setEdit(!edit)}>
+                <Button
+                  variant="light"
+                  className="mr-2 text-danger border-danger font-weight-bold"
+                  onClick={() => setEdit(!edit)}
+                >
                   Cancel
                 </Button>
               </div>
