86038e08f25ebd0a4bb4fcd00ddd2232b3732b0f
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-05 00:52:15 +0100

    Admin page

--- src/components/addContent/lyrics/LyricsAdminPage.jsx
+++ src/components/addContent/lyrics/LyricsAdminPage.jsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 import diff from "simple-text-diff";
 import ContentEditable from "react-contenteditable";
-import { Container, Button, Col, Row } from "react-bootstrap";
+import { Container, Button, Col, Row, Modal } from "react-bootstrap";
 import "./add-lyrics.css";
 import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
 
 const LyricsAdminPage = ({ match }) => {
   const [officialL, setOfficialL] = useState();
-  const [edit, setEdit] = useState(true);
+  // const [edit, setEdit] = useState(true);
   const [lyrics, setLyrics] = useState([]);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     const getEditedLyrics = async () => {
@@ -37,7 +41,21 @@ const LyricsAdminPage = ({ match }) => {
 
   console.log("of-Lyc", officialL);
   return (
-    <Container className="mb-5">
+    <Container className="my-5">
+      {/* <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal> */}
       <Row>
         <Col>
           {lyrics.length === 0 ? (
@@ -60,13 +78,16 @@ const LyricsAdminPage = ({ match }) => {
                       className="mr-4 bg-light"
                       html={
                         diff.diffPatch(lyr.officialLyric, edited.updatedLyric)
-                          .after
+                          .before
                       }
                       disabled={true}
                       // onChange={(e) => setOfficialL(e.target.value)}
                     />
                     <div className="mb-2">
-                      <Link to={`/edited/${lyr._id}/lyrics/${edited._id}`}>
+                      <Link
+                        to={`/edited/${lyr._id}/lyrics/${edited._id}`}
+                        onClick={handleShow}
+                      >
                         <Button className="mr-2">Approve/Reject</Button>
                       </Link>
                     </div>
