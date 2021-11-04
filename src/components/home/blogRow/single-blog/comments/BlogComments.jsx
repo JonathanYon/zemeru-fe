2f72a4f9a94073b5e28c8a38f354b02adff76cd5
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-04 01:53:24 +0100

    this is the blogComment
    the prevouse was blogContent

---
+++ src/components/home/blogRow/single-blog/comments/BlogComments.jsx
@@ -0,0 +1,99 @@
+import { useState, useEffect } from "react";
+import BlogOneComment from "./BlogOneComment";
+import { withRouter } from "react-router";
+import { Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { getBlogComments } from "../../../../../Redux/action";
+
+const BlogComments = (props) => {
+  const [comment, setComment] = useState("");
+  const [commentClick, setCommentClick] = useState(false);
+  const comments = useSelector((state) => state.blogsComments.comments);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const { id } = props.match.params;
+    dispatch(getBlogComments(id));
+  }, []);
+
+  const submitComment = async (e) => {
+    e.preventDefault();
+    const { id } = props.match.params;
+    const payload = {
+      comment,
+    };
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_URL}/blogs/post/${id}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${window.localStorage.getItem("Token")}`,
+          },
+          method: "POST",
+          body: JSON.stringify(payload),
+        }
+      );
+      if (response.ok) {
+        // alert("commented");
+        const resp = await response.json();
+        comments.unshift(resp);
+        setComment("");
+      } else {
+        alert("something Wrong");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    // <div className="row d-flex justify-content-center">
+    //   <div>
+    <div
+      className="card shadow-0 border"
+      style={{ backgroundColor: "#f0f2f5" }}
+    >
+      <div className="card-body p-4">
+        <div className="form-outline mb-4">
+          <form onSubmit={submitComment}>
+            <input
+              type="text"
+              id="addANote"
+              className="form-control"
+              placeholder="Type comment..."
+              value={comment}
+              onClick={() => setCommentClick(true)}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            {commentClick && (
+              <Button
+                className="form-label mt-2 mr-2 text-dark bg-light font-weight-bold"
+                for="addANote"
+                type="submit"
+              >
+                comment
+              </Button>
+            )}
+            {commentClick && (
+              <Button
+                className="form-label mt-2 text-black-50 bg-light font-weight-bold"
+                for="addANote"
+                type="submit"
+                onClick={() => setCommentClick(false)}
+              >
+                cancel
+              </Button>
+            )}
+          </form>
+        </div>
+        {comments.map((aComment) => (
+          <BlogOneComment comment={aComment} key={aComment._id} />
+        ))}
+      </div>
+    </div>
+    //   </div>
+    // </div>
+  );
+};
+export default withRouter(BlogComments);
