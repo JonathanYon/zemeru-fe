6c8fafe34e7eb61bdb3139d9f1dc7435bbd33b7d
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-09 18:08:15 +0100

    stats

---
+++ src/components/me/card/StatsCard.jsx
@@ -0,0 +1,39 @@
+import { Card, Container, Row, Col } from "react-bootstrap";
+import "./left-card.css";
+import { MdComment } from "react-icons/md";
+import { IoNewspaper } from "react-icons/io5";
+import { GrEdit } from "react-icons/gr";
+
+import { useState } from "react";
+
+const StatsCard = (props) => {
+  return (
+    <Card className="left-card mt-5 py-3">
+      <Container>
+        <Row>
+          <Col className="d-flex justify-content-lg-between">
+            <div className="mr-0 d-flex flex-column">
+              <div>
+                5<MdComment className="ml-3 stat-icons" />
+              </div>
+              <small className="icon-name">Comment/Suggestion</small>
+            </div>
+            <div className="mr-0 d-flex flex-column">
+              <div>
+                5<IoNewspaper className="ml-3 stat-icons" />
+              </div>
+              <small className="icon-name">Transcription</small>
+            </div>
+            <div className="mr-0 d-flex flex-column">
+              <div>
+                5<GrEdit className="ml-3 stat-icons" />
+              </div>
+              <small className="icon-name">Lyrics Edit</small>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Card>
+  );
+};
+export default StatsCard;
