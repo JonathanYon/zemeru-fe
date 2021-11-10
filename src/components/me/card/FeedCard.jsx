33f912ab4ada956b132af4f4884bdc01f8a21220
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-10 01:24:28 +0100

    my feed with all my activities

--- src/components/me/card/FeedCard.jsx
+++ src/components/me/card/FeedCard.jsx
@@ -6,39 +6,122 @@ import { IoNewspaper } from "react-icons/io5";
 import { GrEdit } from "react-icons/gr";
 import { FaRegCalendarAlt } from "react-icons/fa";
 
-import { useState } from "react";
+const FeedCard = ({ comment, lyrIwrite, blogComment }) => {
+  //   console.log(lyrIwrite)
 
-const FeedCard = (props) => {
   return (
     <>
-      <Card className="left-card mt-5 py-3">
-        <Container>
-          <Row>
-            <Col className="d-flex justify-content-lg-between">
-              <div className="d-flex">
-                <div className="mr-3">
-                  <MdComment />
-                </div>
-                <div className="text-left mr-5">
-                  <div>You commented on a Lyrics or Blog</div>
-                  <div>
-                    <strong>Title of the Lyrics or Blog</strong>
+      <>
+        {comment && (
+          <Card className="left-card mt-1 py-3">
+            <Container>
+              <Row>
+                <Col className="d-flex justify-content-lg-between">
+                  <div className="d-flex">
+                    <div className="mr-3">
+                      <MdComment className="text-dark" />
+                    </div>
+                    <div className="text-left mr-5">
+                      <div>You commented on {comment.title} Lyrics</div>
+
+                      <div>
+                        <strong>Lyrics Title {comment.title}</strong>
+                      </div>
+                      <div>
+                        {comment.comments.map((com, i) => (
+                          <Badge variant="success" className="mr-1" key={i + 1}>
+                            {com}
+                          </Badge>
+                        ))}
+                      </div>
+                    </div>
                   </div>
-                  <div>
-                    <Badge variant="success">my comment</Badge>
+                  <div className="d-flex flex-column">
+                    <FaRegCalendarAlt className="mb-1 ml-3" />
+                    <span className="feed-date">
+                      {formatDistanceToNow(new Date(2021, 11, 7))}
+                    </span>
                   </div>
-                </div>
-              </div>
-              <div className="d-flex flex-column">
-                <FaRegCalendarAlt className="mb-1 ml-3" />
-                <span className="feed-date">
-                  {formatDistanceToNow(new Date(2021, 11, 7))}
-                </span>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </Card>
+                </Col>
+              </Row>
+            </Container>
+          </Card>
+        )}
+      </>
+      {lyrIwrite && (
+        <>
+          {lyrIwrite.map((song, i) => (
+            <Card className="left-card mt-1 py-3" key={i + 1}>
+              <Container>
+                <Row>
+                  <Col className="d-flex justify-content-lg-between">
+                    <div className="d-flex">
+                      <div className="mr-3">
+                        <IoNewspaper className="text-primary" />
+                      </div>
+                      <div className="text-left mr-5">
+                        <div>
+                          You transcribed {song.title} by {song.artist}
+                        </div>
+
+                        <div>
+                          <strong>Lyrics Title {song.title}</strong>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="d-flex flex-column">
+                      <FaRegCalendarAlt className="mb-1 ml-3" />
+                      <span className="feed-date">
+                        {formatDistanceToNow(new Date(2021, 11, 7))}
+                      </span>
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            </Card>
+          ))}
+        </>
+      )}
+      <>
+        {blogComment && (
+          <Card className="left-card mt-1 py-3">
+            <Container>
+              <Row>
+                <Col className="d-flex justify-content-lg-between">
+                  <div className="d-flex">
+                    <div className="mr-3">
+                      <MdComment className="text-dark" />
+                    </div>
+                    <div className="text-left mr-5">
+                      <div>You commented on {blogComment.author}'s article</div>
+
+                      <div>
+                        <strong>
+                          {blogComment.author} discusses about{" "}
+                          {blogComment.title} in this article.
+                        </strong>
+                      </div>
+                      <div>
+                        {blogComment.comments.map((com, i) => (
+                          <Badge variant="success" className="mr-1" key={i + 1}>
+                            {com}
+                          </Badge>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="d-flex flex-column">
+                    <FaRegCalendarAlt className="mb-1 ml-3" />
+                    <span className="feed-date">
+                      {formatDistanceToNow(new Date(2021, 11, 7))}
+                    </span>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </Card>
+        )}
+      </>
     </>
   );
 };
