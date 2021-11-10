afb923690ee75fd4769bdca43781920d4b2fce5e
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-10 18:29:59 +0100

    users stats

---
+++ src/components/home/charts/user-chart/UserStats.jsx
@@ -0,0 +1,42 @@
+import { Card, Container, Row, Col } from "react-bootstrap";
+import "../chart.css";
+import { MdComment } from "react-icons/md";
+import { IoNewspaper } from "react-icons/io5";
+import { GrEdit } from "react-icons/gr";
+
+import { useState } from "react";
+
+const UserStatsCard = ({ commentNumber, lyricNum, editNum }) => {
+  return (
+    <Card className="left-card mt-5 py-3">
+      <Container>
+        <Row>
+          <Col className="d-flex justify-content-lg-between">
+            <div className="mr-0 d-flex flex-column">
+              <div>
+                {commentNumber}
+                <MdComment className="ml-3 stat-icons" />
+              </div>
+              <small className="icon-name">Comment/Suggestion</small>
+            </div>
+            <div className="mr-0 d-flex flex-column">
+              <div>
+                {lyricNum}
+                <IoNewspaper className="ml-3 stat-icons" />
+              </div>
+              <small className="icon-name">Transcription</small>
+            </div>
+            <div className="mr-0 d-flex flex-column">
+              <div>
+                {editNum}
+                <GrEdit className="ml-3 stat-icons" />
+              </div>
+              <small className="icon-name">Lyrics Edit</small>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Card>
+  );
+};
+export default UserStatsCard;
