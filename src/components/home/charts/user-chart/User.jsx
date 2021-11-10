f3d4e862e1b62c18e22e7916ea92e8db6e4c2aba
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-10 18:28:50 +0100

    user page

---
+++ src/components/home/charts/user-chart/User.jsx
@@ -0,0 +1,190 @@
+import { useEffect, useState } from "react";
+import { Container, Row, Col, Button, Badge } from "react-bootstrap";
+import "../chart.css";
+import UserBio from "./UserBio";
+import UserFeedCard from "./UserFeedCard";
+import UserStatsCard from "./UserStats";
+
+const User = ({ match }) => {
+  const [errors, setErrors] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [oneUser, setOneUser] = useState(null);
+  const [myComments, setMyComments] = useState(null);
+  const [myCommentsBlog, setMyCommentsBlog] = useState(null);
+
+  useEffect(() => {
+    const getMyBlogComments = async () => {
+      const { id } = match.params;
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_URL}/users/${id}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${window.localStorage.getItem("Token")}`,
+            },
+          }
+        );
+        if (response.ok) {
+          const res = await response.json();
+          setOneUser(res);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMyBlogComments();
+  }, []);
+  console.log("user", oneUser);
+
+  useEffect(() => {
+    const getMyComments = async () => {
+      const { id } = match.params;
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_URL}/users/lyrics/comments/${id}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${window.localStorage.getItem("Token")}`,
+            },
+          }
+        );
+        if (response.ok) {
+          const res = await response.json();
+          setMyComments(res);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMyComments();
+  }, []);
+  console.log("myComments", myComments);
+  const noOfComments = myComments?.commAndID
+    .map((ele) => ele.comments)
+    .flat().length;
+  const noOfEdits = myComments?.myEdits.filter(
+    (ele) => ele.userId === oneUser?._id
+  ).length;
+
+  useEffect(() => {
+    const getMyBlogComments = async () => {
+      const { id } = match.params;
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_URL}/users/blogs/comments/${id}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${window.localStorage.getItem("Token")}`,
+            },
+          }
+        );
+        if (response.ok) {
+          const res = await response.json();
+          setMyCommentsBlog(res);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMyBlogComments();
+  }, []);
+  console.log("myCommentblog-", myCommentsBlog);
+
+  return (
+    <>
+      <Container
+        fluid
+        className=" big-info-container"
+        style={{
+          backgroundImage: `url(${oneUser?.avatar})`,
+          backgroundRepeat: "repeat-x",
+          backgroundSize: "100% 100%",
+          objectFit: "cover",
+        }}
+      >
+        <Container className="info-container">
+          <Row style={{ height: "40vh" }}>
+            <Col>
+              <Col className="position-relative d-flex">
+                <div className="text-white mt-5 d-flex">
+                  <img
+                    src={oneUser?.avatar}
+                    alt=""
+                    className="mezmur-cover mt-5 rounded-circle"
+                    style={{ height: "14vw", width: "14vw" }}
+                  />
+                </div>
+              </Col>
+            </Col>
+            <Col className="mt-5 position-relative">
+              <div className="follow d-flex mt-5">
+                <div className="mt-4 text-white mr-5">
+                  <strong>Featured</strong>
+                </div>
+                <div className="mt-4 text-white mr-3">
+                  <strong className="d-flex">
+                    <span className="text-warning mr-1">
+                      {oneUser?.followers.length}
+                    </span>
+                    <span>Followers</span>
+                  </strong>
+                </div>
+                <div className="mt-4 text-white mr-3">
+                  <strong className="d-flex">
+                    <span className="text-warning mr-1">
+                      {oneUser?.following.length}
+                    </span>
+                    <span>Following</span>
+                  </strong>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+
+      <Container className="mt-5 mb-5">
+        <Row>
+          <Col xs={4}>
+            <h5>
+              @{oneUser?.username}
+              <Badge variant="warning ml-1">{oneUser?.token}</Badge>
+            </h5>
+            <Button
+              className="bg-light text-black-50 mb-2"
+              //   onClick={handleShow}
+            >
+              Follow
+            </Button>
+            <UserBio bio={oneUser?.bio} />
+            <UserStatsCard
+              commentNumber={noOfComments}
+              lyricNum={myComments?.lyrics.length}
+              editNum={noOfEdits}
+            />
+          </Col>
+          <Col xs={8}>
+            {myComments?.commAndID.map((comment) => (
+              <UserFeedCard
+                comment={comment}
+                key={comment.id}
+                user={oneUser?.username}
+              />
+            ))}
+            {myCommentsBlog?.map((comment) => (
+              <UserFeedCard blogComment={comment} key={comment.id} />
+            ))}
+            {myComments?.lyrics &&
+              myComments?.lyrics?.map((lyr) => (
+                <UserFeedCard lyrIwrite={lyr} user={oneUser?.username} />
+              ))}
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+export default User;
